f3eb9244b1906fdcc1346af2afa348187642d77b
amira <ammou35@ulaval.ca>
2025-01-13 18:02:48 -0500

    ajout des éléments du header avec style temporaire

---
+++ src/components/header/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.css';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header-title">Amira Mouamine</h1>
+            <nav className="header-nav">
+                <a href="#about-me" className="header-link">About Me</a>
+                <a href="#projects" className="header-link">Projects</a>
+            </nav>
+        </header>
+    );
+}
+
+
+export default Header;
